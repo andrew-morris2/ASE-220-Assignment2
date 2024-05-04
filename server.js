cf91bcd546787bfb7f70cc3c41e207c899432869
Andrew Morris <andrew.morris0202@gmail.com>
2024-05-03 23:49:21 -0400

    Commit

--- server.js
+++ server.js
@@ -2,7 +2,47 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 
+const url='mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+async function main() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+};
+
+async function importUsers(credentialsFilename, collectionName) {
+    try {
+        // adding the credentials from credentials.json into Mongodb
+        const credentialsData = fs.readFileSync(credentialsFilename, 'utf8');
+        const credentials = JSON.parse(credentialsData);
+
+        
+        const users = Object.entries(credentials).map(([username, password]) => ({ username, password }));
+
+        
+        const dbName = 'mydatabase';
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+
+        
+        const result = await collection.insertMany(users);
+        console.log(`${result.insertedCount} users inserted into ${collectionName} collection`);
+    } catch (error) {
+        console.error('Error importing users from JSON:', error);
+    }
+};
+
+main();
+const credentials = 'credentials';
+importUsers("credentials.json", credentials);
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------
 const app=express();
 
 app.use(bodyParser.json());
@@ -112,6 +152,33 @@ app.delete('/api/blobs/:id', (req, res) =>{ //delete requests
             })
         }
     });
+});
+
+
+
+app.post('/api/signin', async (req, res) =>{
+    const { email, password} = req.body;
+    
+        try {
+            const dbName = 'mydatabase';
+            const db = client.db(dbName);
+            const collection = db.collection('credentials');
+    
+            const user = await collection.findOne({ email: email, password: password });
+    
+            if (user) {
+                // User authenticated
+                res.status(200).json({ status: 'success', message: 'Authentication successful' });
+                console.log('User authenticated:', email);
+            } else {
+                // User not found or invalid credentials
+                res.status(401).json({ status: 'error', message: 'Invalid email or password' });
+                console.log('Invalid email or password');
+            }
+        } catch (error) {
+            console.error('Error handling sign-in:', error);
+            res.status(500).json({ status: 'error', message: 'Internal server error' });
+        }
 });
 
 
